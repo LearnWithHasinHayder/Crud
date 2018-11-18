daaa2210b7b7ecb5490e8d35a517fbbd567a9b86
Hasin Hayder <countdraculla@gmail.com>
2018-11-19 03:20:46 +0600

    add edit

--- inc/functions.php
+++ inc/functions.php
@@ -53,7 +53,7 @@ function generateReport() {
             <tr>
                 <td><?php printf( '%s %s', $student['fname'], $student['lname'] ); ?></td>
                 <td><?php printf( '%s', $student['roll'] ); ?></td>
-                <td><?php printf( '<a href="/crud/index.php?task=edit&id=%s">Edit</a> | <a href="/crud/index.php?task=delete&id=%s">Delete</a>',$student['id'],$student['id'] ); ?></td>
+                <td><?php printf( '<a href="/crud/index.php?task=edit&id=%s">Edit</a> | <a href="/crud/index.php?task=delete&id=%s">Delete</a>', $student['id'], $student['id'] ); ?></td>
             </tr>
             <?
         }
@@ -62,3 +62,67 @@ function generateReport() {
     </table>
     <?php
 }
+
+function addStudent( $fname, $lname, $roll ) {
+    $found          = false;
+    $serialziedData = file_get_contents( DB_NAME );
+    $students       = unserialize( $serialziedData );
+    foreach ( $students as $_student ) {
+        if ( $_student['roll'] == $roll ) {
+            $found = true;
+            break;
+        }
+    }
+    if ( ! $found ) {
+        $newId   = count( $students ) + 1;
+        $student = array(
+            'id'    => $newId,
+            'fname' => $fname,
+            'lname' => $lname,
+            'roll'  => $roll
+        );
+        array_push( $students, $student );
+        $serializedData = serialize( $students );
+        file_put_contents( DB_NAME, $serializedData, LOCK_EX );
+
+        return true;
+    }
+
+    return false;
+}
+
+function getStudent( $id ) {
+    $serialziedData = file_get_contents( DB_NAME );
+    $students       = unserialize( $serialziedData );
+    foreach ( $students as $student ) {
+        if ( $student['id'] == $id ) {
+            return $student;
+        }
+    }
+
+    return false;
+}
+
+function updateStudent( $id, $fname, $lname, $roll ) {
+    $found          = false;
+    $serialziedData = file_get_contents( DB_NAME );
+    $students       = unserialize( $serialziedData );
+    foreach ( $students as $_student ) {
+        if ( $_student['roll'] == $roll && $_student['id'] != $id ) {
+            $found = true;
+            break;
+        }
+    }
+    if ( ! $found ) {
+        $students[ $id - 1 ]['fname'] = $fname;
+        $students[ $id - 1 ]['lname'] = $lname;
+        $students[ $id - 1 ]['roll']  = $roll;
+        $serializedData               = serialize( $students );
+        file_put_contents( DB_NAME, $serializedData, LOCK_EX );
+
+        return true;
+    }
+
+    return false;
+}
+
--- index.php
+++ index.php
@@ -1,10 +1,42 @@
 <?php
 require_once "inc/functions.php";
-$info = '';
-$task = $_GET['task'] ?? 'report';
+$info  = '';
+$task  = $_GET['task'] ?? 'report';
+$error = $_GET['error'] ?? '0';
 if ( 'seed' == $task ) {
     seed();
     $info = "Seeding is complete";
+}
+$fname = '';
+$lname = '';
+$roll  = '';
+if ( isset( $_POST['submit'] ) ) {
+    $fname = filter_input( INPUT_POST, 'fname', FILTER_SANITIZE_STRING );
+    $lname = filter_input( INPUT_POST, 'lname', FILTER_SANITIZE_STRING );
+    $roll  = filter_input( INPUT_POST, 'roll', FILTER_SANITIZE_STRING );
+    $id = filter_input( INPUT_POST, 'id', FILTER_SANITIZE_STRING );
+    if($id){
+        //update the existing student
+        if ( $fname != '' && $lname != '' && $roll != '' ) {
+            $result = updateStudent($id, $fname, $lname, $roll);
+            if ( $result ) {
+                header( 'location: /crud/index.php?task=report' );
+            } else {
+                $error = 1;
+            }
+        }
+    }else{
+        //add a new student
+        if ( $fname != '' && $lname != '' && $roll != '' ) {
+            $result = addStudent( $fname, $lname, $roll );
+            if ( $result ) {
+                header( 'location: /crud/index.php?task=report' );
+            } else {
+                $error = 1;
+            }
+        }
+    }
+
 }
 ?>
 
@@ -37,6 +69,15 @@ if ( 'seed' == $task ) {
             ?>
         </div>
     </div>
+    <?php if ( '1' == $error ): ?>
+        <div class="row">
+            <div class="column column-60 column-offset-20">
+                <blockquote>
+                    Duplicate Roll Number
+                </blockquote>
+            </div>
+        </div>
+    <?php endif; ?>
     <?php if ( 'report' == $task ): ?>
         <div class="row">
             <div class="column column-60 column-offset-20">
@@ -44,6 +85,45 @@ if ( 'seed' == $task ) {
             </div>
         </div>
     <?php endif; ?>
+    <?php if ( 'add' == $task ): ?>
+        <div class="row">
+            <div class="column column-60 column-offset-20">
+                <form action="/crud/index.php?task=add" method="POST">
+                    <label for="fname">First Name</label>
+                    <input type="text" name="fname" id="fname" value="<?php echo $fname; ?>">
+                    <label for="lname">Last Name</label>
+                    <input type="text" name="lname" id="lname" value="<?php echo $lname; ?>">
+                    <label for="roll">Roll</label>
+                    <input type="number" name="roll" id="roll" value="<?php echo $roll; ?>">
+                    <button type="submit" class="button-primary" name="submit">Save</button>
+                </form>
+            </div>
+        </div>
+    <?php endif; ?>
+    <?php
+    if ( 'edit' == $task ):
+        $id = filter_input( INPUT_GET, 'id', FILTER_SANITIZE_STRING );
+        $student = getStudent( $id );
+        if ( $student ):
+            ?>
+            <div class="row">
+                <div class="column column-60 column-offset-20">
+                    <form method="POST">
+                        <input type="hidden" value="<?php echo $id ?>" name="id">
+                        <label for="fname">First Name</label>
+                        <input type="text" name="fname" id="fname" value="<?php echo $student['fname']; ?>">
+                        <label for="lname">Last Name</label>
+                        <input type="text" name="lname" id="lname" value="<?php echo $student['lname']; ?>">
+                        <label for="roll">Roll</label>
+                        <input type="number" name="roll" id="roll" value="<?php echo $student['roll']; ?>">
+                        <button type="submit" class="button-primary" name="submit">Update</button>
+                    </form>
+                </div>
+            </div>
+        <?php
+        endif;
+    endif;
+    ?>
 </div>
 </body>
 </html>
